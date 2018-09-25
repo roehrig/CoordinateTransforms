57d9a06b1717a04c35b535f4b51bd7abf07c3022
fabricio s. Marin <fabricio.s.marin@gmail.com>
2018-09-25 18:10:21 -0500

    Added more buttons and default scan parameter entries clicking 'build scan' button populates table in 'create script' window. fixed pop-up window bug.

--- src/CoarseScanWidget.py
+++ src/CoarseScanWidget.py
@@ -41,6 +41,7 @@ import h5py
 import numpy as np
 import string
 from XRF_Boundary import *
+from CreateScriptWidget import *
 from CheckBoxDialog import QMessageBoxWithCheckBox
 from os.path import expanduser
 
@@ -58,9 +59,16 @@ class CoarseScanWidget(QWidget):
     def __init__(self, parent, path=None):
         super(CoarseScanWidget, self).__init__()
 
+        # self.parent = CreateScriptWidget()
+        self.parent = parent
         self.scan_boundary = XRFBoundary()
         self.file_path = None
         self.file_list = None
+        self.scan_params = None
+        self.scan_params2 = None
+        self.x_pixel_size = None
+        self.y_pixel_size = None
+        self.dwell_time = None
 
         self.tree_view = QTreeView()
         self.list_view = QListWidget()
@@ -114,11 +122,26 @@ class CoarseScanWidget(QWidget):
         self.show_plots_button.setToolTip('Show plots with calculated boundaries.')
         self.show_plots_button.clicked.connect(self.on_show_plots_button_click)
 
-        self.label_theta = QLabel('Delta Theta')
-        self.label_theta.setAlignment(Qt.AlignRight)
-        self.text_theta = QLineEdit()
-        self.text_theta.setMinimumWidth(80)
-        self.text_theta.setMaximumSize(100, 20)
+        self.label_x_size = QLabel('X step size (um)')
+        self.label_x_size.setAlignment(Qt.AlignRight)
+        self.text_x_size = QLineEdit('1')
+        self.text_x_size.setMinimumWidth(80)
+        self.text_x_size.setMaximumSize(100, 20)
+        # self.text_x_size.textChanged.connect(self.enable_build_scan_button)
+
+        self.label_y_size = QLabel('Y step size (um)')
+        self.label_y_size.setAlignment(Qt.AlignRight)
+        self.text_y_size = QLineEdit('1')
+        self.text_y_size.setMinimumWidth(80)
+        self.text_y_size.setMaximumSize(100, 20)
+        # self.text_y_size.textChanged.connect(self.enable_build_scan_button)
+
+        self.label_dwell = QLabel('Dwell time (ms)')
+        self.label_dwell.setAlignment(Qt.AlignRight)
+        self.text_dwell = QLineEdit('50')
+        self.text_dwell.setMinimumWidth(80)
+        self.text_dwell.setMaximumSize(100, 20)
+        # self.text_dwell.textChanged.connect(self.enable_build_scan_button)
 
         self.label_element = QLabel('Element')
         self.label_element.setAlignment(Qt.AlignRight)
@@ -132,6 +155,12 @@ class CoarseScanWidget(QWidget):
         self.text_coefficient = QLineEdit()
         self.text_coefficient.setMinimumWidth(80)
         self.text_coefficient.setMaximumSize(100, 20)
+
+        self.label_theta = QLabel('Delta Theta')
+        self.label_theta.setAlignment(Qt.AlignRight)
+        self.text_theta = QLineEdit()
+        self.text_theta.setMinimumWidth(80)
+        self.text_theta.setMaximumSize(100, 20)
 
         self.label_stage_pv = QLabel('Rotation Stage PV')
         self.label_stage_pv.setAlignment(Qt.AlignRight)
@@ -157,14 +186,22 @@ class CoarseScanWidget(QWidget):
         grid_layout.addWidget(self.text_num_files, 0, 1)
         grid_layout.addWidget(self.show_plots_button, 1, 1)
         grid_layout.addWidget(self.build_scan_button, 2, 1)
-        grid_layout.addWidget(self.label_element, 0, 2)
-        grid_layout.addWidget(self.label_coefficient, 1, 2)
-        grid_layout.addWidget(self.label_theta, 2, 2)
-        grid_layout.addWidget(self.label_stage_pv, 3, 2)
-        grid_layout.addWidget(self.text_element, 0, 3)
-        grid_layout.addWidget(self.text_coefficient, 1, 3)
-        grid_layout.addWidget(self.text_theta, 2, 3)
-        grid_layout.addWidget(self.text_stage_pv, 3, 3)
+
+        grid_layout.addWidget(self.label_x_size, 0, 2)
+        grid_layout.addWidget(self.label_y_size, 1, 2)
+        grid_layout.addWidget(self.label_dwell, 2, 2)
+        grid_layout.addWidget(self.text_x_size, 0, 3)
+        grid_layout.addWidget(self.text_y_size, 1, 3)
+        grid_layout.addWidget(self.text_dwell, 2, 3)
+
+        grid_layout.addWidget(self.label_element, 0, 4)
+        grid_layout.addWidget(self.label_coefficient, 1, 4)
+        grid_layout.addWidget(self.label_theta, 2, 4)
+        grid_layout.addWidget(self.label_stage_pv, 3, 4)
+        grid_layout.addWidget(self.text_element, 0, 5)
+        grid_layout.addWidget(self.text_coefficient, 1, 5)
+        grid_layout.addWidget(self.text_theta, 2, 5)
+        grid_layout.addWidget(self.text_stage_pv, 3, 5)
 
         widget_layout = QVBoxLayout()
         widget_layout.addLayout(files_hbox)
@@ -202,7 +239,7 @@ class CoarseScanWidget(QWidget):
 
         stage_pv = self.text_stage_pv.text()
 
-        if self.file_path is None and stage_pv is '':
+        if self.file_path is None or stage_pv is '':
             err_msg = QMessageBox()
             err_msg.setIcon(QMessageBox.Critical)
             err_msg.setText('Missing Information')
@@ -220,6 +257,18 @@ class CoarseScanWidget(QWidget):
             num_files = len(self.scan_boundary.get_hdf_file_list())
             self.text_num_files.setText('{} Files Selected'.format(num_files))
 
+
+        if num_files <= 2:
+            err_msg = QMessageBox()
+            err_msg.setIcon(QMessageBox.Critical)
+            err_msg.setText('Missing Information')
+            err_msg.setInformativeText('less than 3 files selected')
+            err_msg.setDetailedText('Please make sure more than 2 files have been selected in the browser window')
+            err_msg.setStandardButtons(QMessageBox.Ok)
+
+            ret_val = err_msg.exec_()       
+        else:   
+            return
         return
 
     def on_select_element_button_click(self):
@@ -240,19 +289,46 @@ class CoarseScanWidget(QWidget):
 
         coefficient = int(self.text_coefficient.text())
         element = self.text_element.text()
+        dtheta = self.text_theta.text()
+        dtheta = float(dtheta)
         element_index = self.scan_boundary.get_element_index(element)
         self.scan_boundary.calc_xy_bounds(coefficient, element_index)
+        self.scan_boundary.interpolate_bounds(dtheta)
+        self.scan_params = self.scan_boundary.get_boundaries()
 
         self.show_plots_button.setDisabled(False)
         self.build_scan_button.setDisabled(False)
-
+        # self.enable_build_scan_button()
         return
 
     def on_build_scan_button_click(self):
+        
+        self.scan_params2 = []
+        self.x_pixel_size = self.text_x_size.text()
+        self.y_pixel_size = self.text_y_size.text()
+        self.dwell_time = self.text_dwell.text()
 
-        coarse_scans = self.scan_boundary.get_image_boundaries()
+        self.parent.file_tab.scan_table.setRowCount(len(self.scan_params))
+        for i in range(len(self.scan_params)): #i is the row number
+            self.parent.file_tab.scan_table.setItem(i,0,QTableWidgetItem(str(self.scan_params[i][1]))) # x center
+            self.parent.file_tab.scan_table.setItem(i,1,QTableWidgetItem(str(self.scan_params[i][3]))) # y center
+            # self.file_tab.scan_table.setItem(i,2,QTableWidgetItem(str(scan_parameters[0]))) # z center
+            self.parent.file_tab.scan_table.setItem(i,3,QTableWidgetItem(str(self.scan_params[i][0]))) # theta
+            self.parent.file_tab.scan_table.setItem(i,4,QTableWidgetItem(str(self.scan_params[i][2]))) # x width
+            self.parent.file_tab.scan_table.setItem(i,5,QTableWidgetItem(str(self.x_pixel_size))) # x step_size
+            self.parent.file_tab.scan_table.setItem(i,6,QTableWidgetItem(str(self.scan_params[i][4]))) # y width
+            self.parent.file_tab.scan_table.setItem(i,7,QTableWidgetItem(str(self.y_pixel_size))) # y step_sze
+            self.parent.file_tab.scan_table.setItem(i,8,QTableWidgetItem(str(self.dwell_time))) # dwell time
 
         return
+
+    # def enable_build_scan_button(self):
+    #     if (self.x_pixel_size is None) or (self.y_pixel_size is None) or \
+    #             (self.dwell_time is None) or (self.scan_params is None):
+    #         return
+    #     else:
+    #         self.build_scan_button.setDisabled(False)
+    #     return
 
     def on_show_plots_button_click(self):
 
